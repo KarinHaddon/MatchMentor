cb014ff41785a4a68df3d5a86495032f2d895607
Karin Haddon <karinlhaddon@gmail.com>
2024-12-30 19:20:02 +0000

    No password HPC

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,9 @@
 import cv2
 from flask import Flask, render_template, request, url_for, jsonify, session, redirect, send_file
 import mysql.connector
-#from mysql.connector import Error
-#from werkzeug.utils import secure_filename
-#import io
 import hashlib
 import os
+import csv 
 
 app = Flask(__name__, static_folder='static')
 app.secret_key = "crabitat"
@@ -103,42 +101,7 @@ def login_submit_press():
         cursor.close()
         conn.close()
         return jsonify({"error": "Please enter both fields"}), 400
-
-@app.route('/addgame', methods=['POST'])
-def add_game():
-    conn = connect()
-    cursor = conn.cursor(dictionary=True)
-
-    userID = session.get('user_id')
-
-    # If no gameID in session, generate a new one
     
-    select_query = "SELECT GamesID FROM games ORDER BY GamesID DESC LIMIT 1"
-    cursor.execute(select_query)
-    result = cursor.fetchone()
-    lGameID = result['GamesID'] if result else 0
-    gameID = int(lGameID) + 1
-    session['gameID'] = gameID  # Save to session for future use
-    
-    # Add the game details
-    game_date = request.form.get('game_date')
-    team1 = request.form.get('team1')
-    team2 = request.form.get('team2')
-
-    if game_date and team1 and team2:
-        insert_game_query = """
-            INSERT INTO games (GamesID, GameDate, Team1, Team2, userID) 
-            VALUES (%s, %s, %s, %s, %s)
-        """
-        cursor.execute(insert_game_query, (gameID, game_date, team1, team2, userID))
-        conn.commit()
-
-        cursor.close()
-        conn.close()
-        
-    return redirect(url_for('homepage'))
-
-
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -153,6 +116,7 @@ def register():
 
 @app.route('/homepage')
 def homepage():
+    session.pop('gameID',None)
     user_id = session.get('user_id')
     username = session.get('username', 'Guest')
     capital_username = username.capitalize()
@@ -187,6 +151,79 @@ def logout():
     session.pop('username',None)
     return redirect(url_for('login'))
 
+@app.route('/addgame', methods=['POST'])
+def add_game():
+    conn = connect()
+    cursor = conn.cursor(dictionary=True)
+
+    gameID = session.get('gameID')
+    userID = session.get('user_id')
+    game_date = request.form.get('game_date')
+    team1 = request.form.get('team1')
+    team2 = request.form.get('team2')
+    video_path = session.get('video_path')
+
+    if game_date and team1 and team2:
+        update_game_query = """
+            UPDATE games 
+            SET GameDate = %s, Team1 = %s, Team2 = %s, userID = %s, videoPath = %s
+            WHERE GamesID = %s
+        """
+        
+        cursor.execute(update_game_query, (game_date, team1, team2, userID, video_path, gameID))
+        conn.commit()
+
+
+        
+        print(f"Used GameID 2: {gameID}")
+
+        labelQ = """
+            SELECT *
+            FROM labelstats
+            WHERE GamesID = %s
+        """
+        cursor.execute(labelQ,(gameID,))
+        labels = cursor.fetchall()
+        print("Labels fetched:", labels)
+
+
+        csvPath = 'static/labels2.csv'
+        images_folder = 'C:/Users/karin/MatchMentor/static/images/'
+        videos_path = 'C:/Users/karin/MatchMentor/'
+        text_path = 'C:/Users/karin/MatchMentor/static/run_training.txt'
+        with open(csvPath, mode ='w') as file:
+            writer = csv.DictWriter(file,fieldnames=labels[0].keys())
+            writer.writeheader()
+            writer.writerows(labels)
+
+        scp_command = f"scp {csvPath} hpc-gw1:~/MatchMentor/data"
+        os.system(scp_command)
+
+        scp_images_command = f"scp {images_folder}* hpc-gw1:~/MatchMentor/data"
+        os.system(scp_images_command)
+        print("All images transmitted")
+
+        print(video_path)
+        video_path = video_path.replace("\\", "/")
+        print(video_path)
+        scp_video_command = f"scp {videos_path}{video_path} hpc-gw1:~/MatchMentor/data"
+        os.system(scp_video_command)
+        print("Video sent")
+
+
+        scp_text_command = f"scp {text_path} hpc-gw1:~/MatchMentor/data"
+        os.system(scp_text_command)
+        print("Sent text file")
+
+        cursor.close()
+        conn.close()
+
+
+        
+        session.pop('video_path', None)
+
+    return redirect(url_for('homepage'))
+
 UPLOAD_FOLDER = 'static/uploads/videos'
 ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'wmv', 'flv'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -201,94 +238,74 @@ def upload_video():
         return 'No selected file', 400
     
     if video:
-        #save the uploaded video to the uploads folder
         filename = video.filename
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         video.save(filepath)
 
-        #path for storing frames in the 'static/images' folder
         images_folder = os.path.join('static', 'images')
         if not os.path.exists(images_folder):
             os.makedirs(images_folder)
 
-        #extract the first frame from the video
         video_capture = cv2.VideoCapture(filepath)
         success, frame = video_capture.read()
         frame_filename = None
         if success:
-            frame_filename = "frame_0.jpg"
+            frame_filename = "frame_0.png"
             frame_path = os.path.join(images_folder, frame_filename)
             cv2.imwrite(frame_path, frame)
         video_capture.release()
 
-        # conn = connect()
-        # cursor = conn.cursor(dictionary=True)
-        # select_query = "SELECT GamesID FROM games ORDER BY GamesID DESC LIMIT 1"
-        # cursor.execute(select_query)
-        # result = cursor.fetchone()
-        # lGameID = result['GamesID'] if result else 0
-        # gameID = int(lGameID) + 1
-
-        # insert_game_query = "INSERT INTO games (GamesID, userID, GameDate, Team1, Team2) VALUES (%s, NULL, NULL, NULL, NULL)"
-        # cursor.execute(insert_game_query, (gameID,))
-        # conn.commit()
-
-        #session['gameID'] = gameID
-
-        #cursor.close()
-        #conn.close()
-
-        #redirect to labelling page, passing the video path and first frame details
+        session['video_path'] = filepath
+        
         return redirect(url_for('labelling', frame_filename=frame_filename, frame_number=1, video_path=filepath))
 
     return 'File upload failed', 500
 
+@app.route('/serve_frame')
+def serve_frame():
+    frame_filename = request.args.get('frame_filename')
+
+    if not frame_filename:
+        return 'Frame filename not provided', 400
+    
+    frame_path = os.path.join(app.config['UPLOAD_FOLDER'], frame_filename)
+    
+    if not os.path.exists(frame_path):
+        return 'Frame not found', 404
+    
+    return send_file(frame_path, mimetype='image/png')
+
 @app.route('/labelling', methods=['GET'])
 def labelling():
+    print("heyyyy")
+    gameID = session.get('gameID')
+    print("GamesID before: " + str(gameID))
     conn = connect()
     cursor = conn.cursor(dictionary=True)
 
     frame_filename = request.args.get('frame_filename')
     frame_number = int(request.args.get('frame_number', 0))
     video_path = request.args.get('video_path')
-
-    gameID = session.get('gameID')
+    userID = session.get('user_id')
+    
 
     if not gameID:
 
-        userID = session.get('user_id')
-
-        # If no gameID in session, generate a new one
-    
         select_query = "SELECT GamesID FROM games ORDER BY GamesID DESC LIMIT 1"
         cursor.execute(select_query)
         result = cursor.fetchone()
-        lGameID = result['GamesID'] if result else 0
-        gameID = int(lGameID) + 1
+        lgameID = result['GamesID'] if result else 0
+        print(f"lgameID: {gameID}")
+        gameID = lgameID + 1
+        print(f"lgameID+1: {gameID}")
+        session['gameID'] = gameID 
 
         insert_game_query = "INSERT INTO games (GamesID, userID, GameDate, Team1, Team2) VALUES (%s, NULL, NULL, NULL, NULL)"
         cursor.execute(insert_game_query, (gameID,))
         conn.commit()
 
-        session['gameID'] = gameID  # Save to session for future use
-    
-        # Add the game details
-        game_date = request.form.get('game_date')
-        team1 = request.form.get('team1')
-        team2 = request.form.get('team2')
-
-        if game_date and team1 and team2:
-            insert_game_query = """
-                INSERT INTO games (GamesID, GameDate, Team1, Team2, userID) 
-                VALUES (%s, %s, %s, %s, %s)
-            """
-            cursor.execute(insert_game_query, (gameID, game_date, team1, team2, userID))
-            conn.commit()
-    
-
     print("GamesID: " + str(gameID))
     
-    # Ensure that frame_path exists for the labelling page
     if frame_filename:
         frame_path = f'images/{frame_filename}'
     else:
@@ -298,21 +315,6 @@ def labelling():
     conn.close()
 
     return render_template('labelling.html', frame_path=frame_path, frame_number=frame_number, video_path=video_path, gameID=gameID)
-
-@app.route('/serve_frame')
-def serve_frame():
-    frame_filename = request.args.get('frame_filename')
-
-    if not frame_filename:
-        return 'Frame filename not provided', 400
-    
-    # Build the full path
-    frame_path = os.path.join(app.config['UPLOAD_FOLDER'], frame_filename)
-    
-    if not os.path.exists(frame_path):
-        return 'Frame not found', 404
-    
-    return send_file(frame_path, mimetype='image/jpeg')
 
 @app.route('/next_frame', methods=['POST'])
 def next_frame():
@@ -328,11 +330,15 @@ def next_frame():
 
     selected_team = request.form.get('selected_team')
     in_play_value = request.form.get('in_play_value')
+    passing_value = request.form.get('passing_value')
+    goal_value = request.form.get('goal_value')
 
     session['selected_team'] = selected_team
     session['in_play_value'] = in_play_value
+    session['passing_value'] = passing_value
+    session['goal_value'] = goal_value
 
-    if selected_team == ('your_team'):
+    if selected_team == ('true'):
         selected_team = 1
     else:
         selected_team = 0
@@ -342,13 +348,23 @@ def next_frame():
     else:
         in_play_value = 0
 
-    print(f"Received data: GameID: {gameID}, Frame Number: {frame_number}, Team: {selected_team}, InPlay: {in_play_value}")
+    if passing_value == ('true'):
+        passing_value = 1
+    else:
+        passing_value = 0
+
+    if goal_value == ('true'):
+        goal_value = 1
+    else:
+        goal_value = 0
+
+    print(f"Received data: GameID: {gameID}, Frame Number: {frame_number}, Team: {selected_team}, InPlay: {in_play_value}, Passing: {passing_value}, Goal: {goal_value}")
 
     insertQ = """
-    INSERT INTO labelstats (GamesID, Frame, Posession, InPlay)
-    VALUES (%s,%s,%s,%s)
+    INSERT INTO labelstats (GamesID, Frame, Posession, InPlay, Passing, Goal)
+    VALUES (%s,%s,%s,%s,%s,%s)
     """
-    cursor.execute(insertQ,(gameID,frame_number,selected_team,in_play_value))
+    cursor.execute(insertQ,(gameID,frame_number,selected_team,in_play_value,passing_value,goal_value))
     conn.commit()
     cursor.close()
     conn.close()
@@ -361,19 +377,17 @@ def next_frame():
     if not os.path.exists(images_folder):
         os.makedirs(images_folder)
 
-    # Open the video file
     video_capture = cv2.VideoCapture(video_path)
-    video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number)  # Move to the desired frame
+    video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number) 
     success, frame = video_capture.read()
 
     frame_filename = None
 
     if success:
-        # Save the frame as an image
-        frame_filename = f"frame_{frame_number}.jpg"
+        frame_filename = f"frame_{frame_number}.png"
         frame_path = os.path.join(images_folder, frame_filename)
         cv2.imwrite(frame_path, frame)
-        frame_number += 10  # Increment the frame number for the next request
+        frame_number += 10 
 
     video_capture.release()
 
@@ -384,29 +398,36 @@ def stats(gameID):
     conn = connect()
     cursor = conn.cursor(dictionary=True)
 
-    # Query to get the game details by gameID
     select_query = """
-    SELECT 
-        GamesID, 
-        GameDate, 
-        Team1, 
-        Team2,
-        ROW_NUMBER() OVER (ORDER BY GameDate) as game_number
-    FROM 
-        games
-    WHERE 
-        GamesID = %s;
+    SELECT GamesID, GameDate, Team1, Team2, videoPath, ROW_NUMBER() OVER (ORDER BY GameDate) as game_number
+    FROM games
+    WHERE GamesID = %s;
     """
     
     cursor.execute(select_query, (gameID,))
-    game = cursor.fetchone()  # Fetch one record
-    print(game)  # Debug: Print the fetched game details
+    game = cursor.fetchone()
+    print(game)  
 
     cursor.close()
     conn.close()
 
     if game:
-        return render_template('stats.html', game=game)
+        video_path = game.get('videoPath', None)
+        if video_path:
+            print(f"Original video path: {video_path}")
+            video_path = video_path.replace('\\', '/')  
+            
+            if not os.path.isabs(video_path):
+                video_path = f"uploads/videos/{os.path.basename(video_path)}"
+
+            video_path = video_path.replace(' ', '%20')
+
+            print(f"Processed video path: {video_path}")
+        else:
+            print("No video path available for this game.")
+            video_path = None  
+        
+        return render_template('stats.html', game=game, video_path=video_path)
     else:
         return "Game not found", 404
 
@@ -417,5 +438,7 @@ def show_frame(frame_path):
     return 
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5500)
+    app.run(debug=True, port=5000)
